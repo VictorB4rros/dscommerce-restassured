6941fae580c67d4e365730d320bf974ab55f81ef
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2026-08-01 22:28:04 -0300

    DSCommerce tests using Rest Assured

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,249 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class ProductControllerRA {
+
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private String clientToken, adminToken, invalidToken;
+    private Long existingProductId, nonExistingProductId;
+    private String productName;
+
+    private Map<String, Object> postProductInstance;
+
+    @BeforeEach
+    public void setUp() {
+        baseURI = "http://localhost:8080";
+
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        invalidToken =  adminToken + "xpto"; // simulates invalid token
+
+        productName = "Macbook";
+
+        postProductInstance = new HashMap<>();
+        postProductInstance.put("name", "Meu novo produto");
+        postProductInstance.put("description", "Lorem ipsum, dolor sit amet consectetur adipisicing elit.");
+        postProductInstance.put("imgUrl", "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+        postProductInstance.put("price", 50.0);
+
+        List<Map<String, Object>> categories = new ArrayList<>();
+        Map<String, Object> category1 = new HashMap<>();
+        category1.put("id", 2);
+        Map<String, Object> category2 = new HashMap<>();
+        category2.put("id", 3);
+
+        categories.add(category1);
+        categories.add(category2);
+
+        postProductInstance.put("categories", categories);
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists() {
+        existingProductId = 2L;
+
+        given()
+                .get("/products/{id}", existingProductId)
+                .then()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Smart TV"))
+                .body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+                .body("price", is(2190.0F))
+                .body("categories.id", hasItems(2, 3))
+                .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+    @Test
+    public void findAllShouldReturnProductPageWhenProductNameIsEmpty() {
+
+        given()
+                .get("/products?page=0")
+                .then()
+                .statusCode(200)
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnProductPageWhenProductNameIsNotEmpty() {
+
+        given()
+                .get("/products?name={productName}", productName)
+                .then()
+                .statusCode(200)
+                .body("content[0].id", is(3))
+                .body("content[0].name", equalTo("Macbook Pro"))
+                .body("content[0].price", is(1250.0F))
+                .body("content[0].imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    @Test
+    public void findAllShouldReturnProductPageWithPriceGreaterThan2000() {
+        given()
+                .get("/products?size=25")
+                .then()
+                .statusCode(200)
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
+    }
+
+    @Test
+    public void insertShouldReturnProductCreatedWhenAdminLogged() {
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Meu novo produto"))
+                .body("price", is(50.0F))
+                .body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"))
+                .body("categories.id", hasItems(2, 3));
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndInvalidName() {
+        postProductInstance.put("name", "ab"); // simulating invalid name
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("Nome precisa ter de 3 a 80 caracteres"));
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndInvalidDescription() {
+        postProductInstance.put("description", "ab"); // simulating invalid description
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("Descrição precisa ter no mínimo 10 caracteres"));
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndPriceIsNegative() {
+        postProductInstance.put("price", -50.0); // simulating negative price
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("O preço deve ser positivo"));
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndPriceIsZero() {
+        postProductInstance.put("price", 0.0); // simulating price equal to zero
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("O preço deve ser positivo"));
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndProductHasNoCategory() {
+        postProductInstance.put("categories", null); // simulating null categories
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("Deve ter pelo menos uma categoria"));
+    }
+
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() {
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenInvalidToken() {
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + invalidToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(401);
+    }
+}
